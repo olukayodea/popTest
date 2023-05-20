b0ace0618b7257ec86b28ac329b3448461d3cc10
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2023-05-20 01:02:37 -0400

    Cleanup

--- scripts/login.php
+++ scripts/login.php
@@ -1,12 +1,10 @@
 <?php
 include_once('../conttrollers/includes.php');
 // Check if the login form is submitted
-// if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Perform the login validation
-    // $users->email = $_POST['email'];
-    // $users->password = $_POST['password'];
-    $users->email = "marguerite@fakemail.com";
-    $users->password = "RandomPassword1";
+    $users->email = $_POST['email'];
+    $users->password = $_POST['password'];
 
     echo json_encode($users->login(), JSON_PRETTY_PRINT);
-// }
+}
